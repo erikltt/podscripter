fd2f9466014126efd2b1552c2194d02bb5ca8f18
erikltt <erikltt@hotmail.com>
2022-02-14 18:09:22 +0100

    First version

---
+++ README.md
@@ -0,0 +1,5 @@
+# podscripter
+Extract informations from podcasts
+## Installation
+Repo : git clone https://github.com/erikltt/podscripter
+VOSK : pip3 install vosk
--- podscripter.py
+++ podscripter.py
@@ -24,7 +24,7 @@ from vosk import Model, KaldiRecognizer, SetLogLevel
 from names_dataset import NameDataset
 
 AUDIO_FILE = "converted.wav"
-TEXT_FILE = "transcripted.txt"
+TEXT_FILE = "transcribed.txt"
 AUDIO_CHUNKS_FOLDER = "audio-chunks"
 SAMPLE_RATE = 16000
 
@@ -40,7 +40,8 @@ IMDB_URLS = Enum("IMDB_URL", [("TITLES", "https://datasets.imdbws.com/title.basi
                               ("RATING", "https://datasets.imdbws.com/title.ratings.tsv.gz")])
 
 
-def download_imdb_dataset(url=IMDB_URLS.TITLES.value):
+def __download_imdb_dataset(url=IMDB_URLS.TITLES.value):
+    """Download IMDB Dataset and store it"""
     print("Downloading data...")
     url_parse = urlparse(url)
     filename = os.path.basename(url_parse.path)
@@ -57,6 +58,7 @@ def download_imdb_dataset(url=IMDB_URLS.TITLES.value):
 
 
 def download_rss_feed():
+    """Download MP3 of an audio podcast coming from RSS feed"""
     feed = feedparser.parse(xml_feed_url)
 
     for entry in feed.entries:
@@ -73,12 +75,8 @@ def download_rss_feed():
         open(folder_file, 'wb').write(r.content)
 
 
-# a function that splits the audio file into chunks
-# and applies speech recognition
-def chunk_wav_file(wav_file, folder=AUDIO_CHUNKS_FOLDER):
-    """
-    Splitting the large audio file into chunks
-    """
+def __chunk_wav_file(wav_file, folder=AUDIO_CHUNKS_FOLDER):
+    """Splitting the large audio file into chunks"""
     # open the audio file using pydub
     sound = AudioSegment.from_wav(wav_file)
     # split audio sound where silence is 700 miliseconds or more and get chunks
@@ -99,7 +97,7 @@ def chunk_wav_file(wav_file, folder=AUDIO_CHUNKS_FOLDER):
     for i, audio_chunk in enumerate(chunks, start=1):
         # export audio chunk and save it in
         # the `folder_name` directory.
-        progress(i, len(chunks), "Writing chunks")
+        __progress(i, len(chunks), "Writing chunks")
         chunk_filename = os.path.join(folder, f"chunk{i:04}.wav")
         audio_chunk.export(chunk_filename, format="wav")
 
@@ -108,7 +106,8 @@ def chunk_wav_file(wav_file, folder=AUDIO_CHUNKS_FOLDER):
     return audio_chunk
 
 
-def vosk_capture(model, recorder, audiofile_path):
+def __vosk_capture(model, recorder, audiofile_path):
+    """Captures sound and convert it to text"""
     SetLogLevel(0)
 
     process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
@@ -122,12 +121,12 @@ def vosk_capture(model, recorder, audiofile_path):
             break
         else:
             recorder.AcceptWaveform(data)
-            # print(recorder.Result())
 
     return json.loads(recorder.FinalResult())["text"]
 
 
-def speed_change(audiofile, speed=1.0):
+def __speed_change(audiofile, speed=1.0):
+    """Change the speed of an audio file to improve voice capture by VOSK"""
     # Manually override the frame_rate. This tells the computer how many
     # samples to play per second
     sound_with_altered_frame_rate = audiofile._spawn(audiofile.raw_data, overrides={
@@ -140,7 +139,8 @@ def speed_change(audiofile, speed=1.0):
     return sound_with_altered_frame_rate.set_frame_rate(audiofile.frame_rate)
 
 
-def sound_convert_to_wav(mp3_filepath):
+def __sound_convert_to_wav(mp3_filepath):
+    """converts MP3 to WAV (needed by VOSK)"""
     filename, file_extension = os.path.splitext(mp3_filepath)
     # convert mp3 to wav
     sound = AudioSegment.from_mp3(mp3_filepath)
@@ -151,7 +151,8 @@ def sound_convert_to_wav(mp3_filepath):
     return wav_file_path
 
 
-def write_line(text_to_write, name_of_file=TEXT_FILE):
+def __write_line(text_to_write, name_of_file=TEXT_FILE):
+    """ Write a line in a file"""
     # Open the file in append & read mode ('a+')
     with open(name_of_file, "a+") as file_object:
         # Move read cursor to the start of file.
@@ -164,7 +165,8 @@ def write_line(text_to_write, name_of_file=TEXT_FILE):
         file_object.write(text_to_write)
 
 
-def progress(count, total, status=''):
+def __progress(count, total, status=''):
+    """Displays a progress bar"""
     bar_len = 60
     filled_len = int(round(bar_len * count / float(total)))
 
@@ -176,6 +178,8 @@ def progress(count, total, status=''):
 
 
 def transcription():
+    """ Transcribe WAVE audio file into text using VOSK library
+    Produce a text file"""
     if not os.path.exists("model"):
         print(
             "Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
@@ -189,22 +193,26 @@ def transcription():
     # filename = time.strftime("%Y%m%d-%H%M%S") + ".txt"
     for i, chunk_filename in enumerate(list_of_chunks, start=1):
         # process each chunk
-        progress(i, len(list_of_chunks), "Transcribing")
-        text_transcripted = vosk_capture(model, rec, chunk_folder + '/' + chunk_filename)
-        if text_transcripted != "":
-            write_line(text_transcripted, filename)
+        __progress(i, len(list_of_chunks), "Transcribing")
+        text_transcribed = __vosk_capture(model, rec, chunk_folder + '/' + chunk_filename)
+        if text_transcribed != "":
+            __write_line(text_transcribed, filename)
 
     print("\nEnded")
 
 
 def conversion():
+    """Convert MP3 to WAV file and split on silence"""
     chunks = []
-    wav_file = sound_convert_to_wav(sound_file_path)
+    wav_file = __sound_convert_to_wav(sound_file_path)
     folder = os.path.splitext(sound_file_path)[0]
-    chunks = chunk_wav_file(wav_file, folder)
+    chunks = __chunk_wav_file(wav_file, folder)
 
 
-def database_extraction():
+def __database_extraction():
+    """Extract all movie title from PL/SQL DB and return a result set
+    We only get films with ratings to avoid being polluted by films with limited diffusion
+    We get the films ordered by length of title desc, for a better match with the transcribed file"""
     # Open connection
     conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s" % (HOST, DATABASE, USER, PASSWORD))
     # Open a cursor to send SQL commands
@@ -220,6 +228,9 @@ def database_extraction():
 
 
 def __match_film(doc, matcher, pattern, position):
+    """ Fine match a film with a specific pattern. Returns the list of matches. The position
+    is the part of the match we want to find so the pattern should not contain any optional criterion
+    (spacy can't tell which part of the pattern corresponds to the match)"""
     match_list = []
     match_text = []
     matcher.add("FILM", [pattern])
@@ -236,8 +247,8 @@ def __match_film(doc, matcher, pattern, position):
     return match_list
 
 
-def __brute_match(rs, transcripted_text):
-    """Brute match the IMDB DB with the transcripted text, to detect film title only with a classic regexp
+def __brute_match(rs, transcribed_text):
+    """Brute match the IMDB DB with the transcribed text, to detect film title only with a classic regexp
     This leads to a lot a false positives but still filters the list for the fine-grained further
     spacy matching process"""
     rs_string = []
@@ -247,8 +258,8 @@ def __brute_match(rs, transcripted_text):
     exception_list = []
     exception_list.append("film")
 
-    # using case folded transcripted text to match without case consideration
-    transcripted_text_casefolded = transcripted_text.casefold()
+    # using case folded transcribed text to match without case consideration
+    transcribed_text_casefolded = transcribed_text.casefold()
 
     # going through the list of title to make a brute match first
     for title in rs[0]:
@@ -258,7 +269,7 @@ def __brute_match(rs, transcripted_text):
         # we convert to brute-detected film name with underscore to make it work with spacy matcher (if we have a space
         # in the title, spacy will consider it as a new token and miss a match)
         if len(text_title) > 2 \
-                and text_title_casefold in transcripted_text_casefolded \
+                and text_title_casefold in transcribed_text_casefolded \
                 and text_title_casefold not in exception_list:
             # we avoid modifying film title containing only one word, it could be a name or surname, and that would
             # interfere with the preparse process that uppercase such words to make spacy recognize them as "PROPN"
@@ -269,12 +280,12 @@ def __brute_match(rs, transcripted_text):
             # we add the brute-matched title to the list of possible real match
             rs_string.append(replaced_title)
             # rewrite the text with this possible match (with _ instead of spaces)
-            transcripted_text = re.sub(r"\b%s\b" % text_title, replaced_title, transcripted_text, flags=re.IGNORECASE)
+            transcribed_text = re.sub(r"\b%s\b" % text_title, replaced_title, transcribed_text, flags=re.IGNORECASE)
 
-    return rs_string, transcripted_text
+    return rs_string, transcribed_text
 
 
-def __fine_match(rs_string, transcripted_text):
+def __fine_match(rs_string, transcribed_text):
     """Fine-matching using spacy matcher and the following matching rules
     MR1 :   {"LOWER": "film"}, {"LOWER": {"IN": rs_string}}, {"POS": "PROPN"}
         --> film vous ne désirez que moi Claire Simon
@@ -291,7 +302,7 @@ def __fine_match(rs_string, transcripted_text):
     nlp = spacy.load('/home/sylvain/.local/lib/python3.8/site-packages/fr_core_news_md/fr_core_news_md-3.2.0')
 
     matcher = Matcher(nlp.vocab)
-    doc = nlp(transcripted_text)
+    doc = nlp(transcribed_text)
     match_list = []
 
     # MR1 : film vous ne désirez que moi claire simon
@@ -368,23 +379,24 @@ def parse():
     3. Brute match with simple regex
     4. Fine match with SPACY matcher"""
     # loading data from input text file
-    with open(transcripted_file) as f:
+    with open(transcribed_file) as f:
         lines = f.readlines()
     f.close()
-    transcripted_text = ' '.join(lines)
+    transcribed_text = ' '.join(lines)
 
     # loading data from DB
-    rs = database_extraction()
+    rs = __database_extraction()
 
     # proceed with brute match
-    rs_string, transcripted_text = __brute_match(rs, transcripted_text)
+    rs_string, transcribed_text = __brute_match(rs, transcribed_text)
 
     # proceed with spacy fine match
-    return __fine_match(rs_string, transcripted_text)
+    return __fine_match(rs_string, transcribed_text)
 
 
 def loadtranslatedtitles_imdb():
-    tsv_filename = download_imdb_dataset(IMDB_URLS.TRANSLATION.value)
+    """Downloads and loads translated movie titles from IMDB downloaded dataset"""
+    tsv_filename = __download_imdb_dataset(IMDB_URLS.TRANSLATION.value)
 
     # Open connection
     conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s" % (HOST, DATABASE, USER, PASSWORD))
@@ -410,7 +422,8 @@ def loadtranslatedtitles_imdb():
 
 
 def loadrating_imdb():
-    tsv_filename = download_imdb_dataset(IMDB_URLS.RATING.value)
+    """Downloads and loads movie ratings from IMDB downloaded dataset"""
+    tsv_filename = __download_imdb_dataset(IMDB_URLS.RATING.value)
 
     # Open connection
     conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s" % (HOST, DATABASE, USER, PASSWORD))
@@ -433,7 +446,8 @@ def loadrating_imdb():
 
 
 def loadmovies_imdb():
-    tsv_filename = download_imdb_dataset()
+    """Downloads and loads movie from IMDB downloaded dataset"""
+    tsv_filename = __download_imdb_dataset()
 
     # Open connection
     conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s" % (HOST, DATABASE, USER, PASSWORD))
@@ -458,16 +472,22 @@ def loadmovies_imdb():
 
 
 def preparse():
+    """ Prepare the transcribed file to be analyzed by SPACY matcher and overwrite it.
+    The only action made for now is to capitalize names and surnames based on philipperemy name-dataset
+    This action is here to provide SPACY matcher a file where names and surnames can be identified and not
+    mingled with adjectives.
+    Uses the top 500 first-names in FR, GB and US. The other nationalities would not be correctly recognized
+    and transcribed by VOSK anyway"""
     nd = NameDataset()
 
-    with open(transcripted_file, "r") as f:
+    with open(transcribed_file, "r") as f:
         lines = f.readlines()
     f.close()
 
-    transcripted_text = ' '.join(lines)
+    transcribed_text = ' '.join(lines)
 
     nlp = spacy.load('/home/sylvain/.local/lib/python3.8/site-packages/fr_core_news_md/fr_core_news_md-3.2.0')
-    doc = nlp(transcripted_text)
+    doc = nlp(transcribed_text)
     # Create list of word tokens after removing stopwords
 
     token_list = []
@@ -492,11 +512,11 @@ def preparse():
     token_list = list(dict.fromkeys(token_list))
 
     for name in token_list:
-        transcripted_text = re.sub(r"\b%s\b" % name, name.capitalize(), transcripted_text)
+        transcribed_text = re.sub(r"\b%s\b" % name, name.capitalize(), transcribed_text)
 
-    with open(transcripted_file, "w") as f:
+    with open(transcribed_file, "w") as f:
         f.truncate(0)
-        f.write(transcripted_text)
+        f.write(transcribed_text)
     f.close()
 
     print("End")
@@ -509,14 +529,14 @@ if __name__ == '__main__':
                                          "conversion", required=True)
     parser.add_argument("--file", help="MP3 to transcript", required="--convert" in sys.argv)
     parser.add_argument("--chunkfolder", help="folder containing chunks", required="--transcript" in sys.argv)
-    parser.add_argument("--transcriptedfile", help="file to pre-parse",
+    parser.add_argument("--transcribedfile", help="file to pre-parse",
                         required="--preparse" in sys.argv or "--parse" in sys.argv)
     parser.add_argument("--xmlfeedurl", help="Feed URL XML format", required="--download" in sys.argv)
     args = parser.parse_args()
     action = args.action
     sound_file_path = args.file
     chunk_folder = args.chunkfolder
-    transcripted_file = args.transcriptedfile
+    transcribed_file = args.transcribedfile
     xml_feed_url = args.xmlfeedurl
 
     if args.action == "convert":
@@ -529,6 +549,7 @@ if __name__ == '__main__':
         print(preparse())
 
     if args.action == "parse":
+        print(preparse())
         print(parse())
 
     if args.action == "download":
